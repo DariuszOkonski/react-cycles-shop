c0aa931ffeceecfd9341ec82f8ab18f0a37b51a7
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 17:45:13 +0100

    add component Article

---
+++ src/Components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <div className='article'>
+      <h2>Title</h2>
+      <h3>Author</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius recusandae sint ut quia id voluptatum, odio distinctio sequi rem quidem esse ab tempora quaerat molestias dolore! Cupiditate odit exercitationem dicta!
+      </p>
+    </div>
+  );
+}
+
+export default Article;
