fef7b081fdf6a0247d00713c5de5b27b694967b5
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 16:28:02 +0100

    import components from folder Pages to component Content

--- src/Layouts/Content.js
+++ src/Layouts/Content.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import StartPage from '../Pages/StartPage';
+import BikesPage from '../Pages/BikesPage';
+import ContactPage from '../Pages/ContactPage';
+import AdminPage from '../Pages/AdminPage';
+import ErrorPage from '../Pages/ErrorPage';
 
 const Content = () => {
   return (
-    <section className='content'>Content</section>
+    <section className='content'>
+      <StartPage />
+      <BikesPage />
+      <ContactPage />
+      <AdminPage />
+      <ErrorPage />
+    </section>
   );
 }
 
