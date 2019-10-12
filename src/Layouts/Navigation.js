7dc2e74dfe3ef99d4ce02ffb5fcb2f3ab2a1fb15
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 15:53:40 +0100

    add Navigation component with jsx structure

--- src/Layouts/Navigation.js
+++ src/Layouts/Navigation.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 const Navigation = () => {
   return (
-    <nav className='navigation'>Navigation</nav>
+    <nav className='navigation'>
+      <ul className='navigation__items'>
+        <li className='navigation__item'>
+          <NavLink exact to='/'>start</NavLink>
+        </li>
+        <li className='navigation__item'>
+          <NavLink exact to='/bikes'>bikes</NavLink>
+        </li>
+        <li className='navigation__item'>
+          <NavLink exact to='/contact'>contact</NavLink>
+        </li>
+        <li className='navigation__item'>
+          <NavLink exact to='/admin'>admin</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
