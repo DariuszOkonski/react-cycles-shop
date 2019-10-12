bb96504450f14335ccc940caf89a0473316129b5
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 12:40:56 +0100

    Updates

---
+++ precache-manifest.f557a6025f28d32c03867119dad41510.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c512fdca57d7266e97375fe1de9ae87",
+    "url": "/react-cycles-shop/index.html"
+  },
+  {
+    "revision": "764d2cf915efa4b52cbe",
+    "url": "/react-cycles-shop/static/js/2.637e1b5d.chunk.js"
+  },
+  {
+    "revision": "25f460c29f90f18fcb7b",
+    "url": "/react-cycles-shop/static/js/main.51bcfb20.chunk.js"
+  },
+  {
+    "revision": "96426a86324a723eab09",
+    "url": "/react-cycles-shop/static/js/runtime-main.9243def4.js"
+  }
+]);
