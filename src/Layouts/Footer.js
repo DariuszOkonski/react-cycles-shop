c6975fbce838067aef1c7c8986947824f05d3a98
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 16:03:21 +0100

    add component Footer

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      Cycles Shop &copy; 2019 Dariusz Okonski
+    </footer>
+  );
+}
+
+export default Footer;
