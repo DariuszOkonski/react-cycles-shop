e5539bcd87c6103611c783414bb7bd067cbe115c
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 13:47:36 +0100

    add Main component to App component

--- src/Layouts/App.js
+++ src/Layouts/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import '../css/style.css';
 import Header from './Header';
+import Main from './Main';
 
 function App() {
   return (
     <div className="app">
       <Header />
-
-
-      <main className='main'>
-        <nav className='navigation'>Navigation</nav>
-        <section className='content'>Content</section>
-      </main>
+      <Main />
 
       <footer className='footer'>
         Footer
