4ce25817f75bbef386a0b0e88c009e6fd3a25d56
Dariusz Okonski <darek200180@gmail.com>
2019-10-12 15:52:36 +0100

    add add Main component to App component

--- src/Layouts/App.js
+++ src/Layouts/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import '../css/style.css';
 import Header from './Header';
 import Main from './Main';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Main />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="app">
+        <Header />
+        <Main />
 
-      <footer className='footer'>
-        Footer
+        <footer className='footer'>
+          Footer
       </footer>
 
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
