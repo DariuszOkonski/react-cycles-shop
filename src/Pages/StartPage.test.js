ec6d98c862718601755408b39532025c99d689f1
Dariusz Okonski <darek200180@gmail.com>
2019-10-14 13:16:21 +0100

    refactorization of a file StartPage.test.js

--- src/Pages/StartPage.test.js
+++ src/Pages/StartPage.test.js
@@ -5,18 +5,36 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * @function setup function return ShallowWrapper
+ * @returns shallow Header component
+ */
+const setup = () => {
+  return shallow(<StartPage />);
+}
+
+/**
+ * @function findDataTest to find component from Header
+ * @param {shallowWrapper} wrapper 
+ * @param {string} comp
+ * @returns {commponent} 
+ */
+const findDataTest = (wrapper, comp) => {
+  return wrapper.find(`[data-test="${comp}"]`);
+}
+
 describe('StartPage.test.js', () => {
 
   test('renders start-wrapper', () => {
-    const wrapper = shallow(<StartPage />);
-    const startWrapper = wrapper.find('[data-test="start-wrapper"]');
+    const wrapper = setup();
+    const startWrapper = findDataTest(wrapper, "start-wrapper");
 
     expect(startWrapper.length).toBe(1);
   })
 
   test('renders article-component', () => {
-    const wrapper = shallow(<StartPage />);
-    const articleComponent = wrapper.find('[data-test="article-component"]');
+    const wrapper = setup();
+    const articleComponent = findDataTest(wrapper, "article-component");
 
     expect(articleComponent.exists()).toBe(true);
   });
