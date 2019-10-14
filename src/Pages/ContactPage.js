d9c3732685ed6a47565bf1ad515da26ca8fbe2ae
Dariusz Okonski <darek200180@gmail.com>
2019-10-14 14:23:37 +0100

    add html structure to ContactPage component

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -3,7 +3,22 @@ import React from 'react';
 const ContactPage = () => {
   return (
     <div className="contact-page">
-      ContactPage
+      <h2
+        className="contact-page__header"
+      >Contact With Us</h2>
+
+      <form className="contact-page__form form">
+
+        <textarea
+          className="form__textarea"
+          placeholder="Type something..."
+        ></textarea>
+
+        <button
+          className="form__button"
+          type="submit"
+        >Send</button>
+      </form>
     </div>
   );
 }
