f4163d85e2819bf0b232c67f152d44a7817cca41
Dariusz Okonski <darek200180@gmail.com>
2019-10-16 19:16:44 +0100

    add new Route component with path /login

--- src/Layouts/Content.js
+++ src/Layouts/Content.js
@@ -5,6 +5,7 @@ import BikesPage from '../Pages/BikesPage';
 import ContactPage from '../Pages/ContactPage';
 import AdminPage from '../Pages/AdminPage';
 import ErrorPage from '../Pages/ErrorPage';
+import LogInPage from '../Pages/LogInPage';
 
 const Content = () => {
   return (
@@ -13,7 +14,8 @@ const Content = () => {
         <Route path='/' exact component={StartPage} />
         <Route path='/bikes' component={BikesPage} />
         <Route path='/contact' component={ContactPage} />
-        <Route path='/admin' component={AdminPage} />
+        <Route path='/login' render={() => <LogInPage />} />
+        <Route path='/admin' render={() => <AdminPage logged={false} />} />
         <Route component={ErrorPage} />
       </Switch>
 
