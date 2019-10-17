a25c0a4bdc835406b7fcd329010ee0cd24b53027
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 12:30:27 +0100

    change component Content into statefull component and write down a logic

--- src/Layouts/Content.js
+++ src/Layouts/Content.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import StartPage from '../Pages/StartPage';
 import BikesPage from '../Pages/BikesPage';
 import ContactPage from '../Pages/ContactPage';
@@ -9,10 +9,33 @@ import LogInPage from '../Pages/LogInPage';
 
 class Content extends Component {
   state = {
-    name: 'dariusz',
-    password: 'okonski',
-    isLogged: false,
+    login: '',
+    password: '',
   }
+
+  handleLogin = (e) => {
+    this.setState({
+      login: e.target.value,
+    })
+  }
+
+  handlePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { login, password } = this.state;
+
+    if (login.length > 0 && password.length > 0) {
+      this.props.onHandleIsLogged();
+    }
+
+  }
+
   render() {
     return (
       <section className='content'>
@@ -20,10 +43,23 @@ class Content extends Component {
           <Route path='/' exact component={StartPage} />
           <Route path='/bikes' component={BikesPage} />
           <Route path='/contact' component={ContactPage} />
-          <Route path='/login' render={() => <LogInPage />} />
-          <Route path='/admin' render={() => <AdminPage {...this.state} />} />
+          <Route path='/login' render={() =>
+            <LogInPage
+              onHandleSubmit={this.handleSubmit}
+              onHandleLogin={this.handleLogin}
+              onHandlePassword={this.handlePassword}
+              login={this.state.login}
+              password={this.state.password}
+            />}
+          />
+          <Route path='/admin' render={() =>
+            <AdminPage login={this.state.login} password={this.state.password} isLogged={this.props.isLogged} />}
+          />
+
           <Route component={ErrorPage} />
         </Switch>
+
+        {this.props.isLogged && <Redirect to='/admin' />}
 
       </section>
     );
