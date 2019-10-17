aa955de16f1bcbff4378dc46b18d6c62240aa3cd
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 10:57:55 +0100

    change stateless component Content into statefull component

--- src/Layouts/Content.js
+++ src/Layouts/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import StartPage from '../Pages/StartPage';
 import BikesPage from '../Pages/BikesPage';
@@ -7,20 +7,27 @@ import AdminPage from '../Pages/AdminPage';
 import ErrorPage from '../Pages/ErrorPage';
 import LogInPage from '../Pages/LogInPage';
 
-const Content = () => {
-  return (
-    <section className='content'>
-      <Switch>
-        <Route path='/' exact component={StartPage} />
-        <Route path='/bikes' component={BikesPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Route path='/login' render={() => <LogInPage />} />
-        <Route path='/admin' render={() => <AdminPage logged={false} />} />
-        <Route component={ErrorPage} />
-      </Switch>
+class Content extends Component {
+  state = {
+    name: 'dariusz',
+    password: 'okonski',
+    isLogged: false,
+  }
+  render() {
+    return (
+      <section className='content'>
+        <Switch>
+          <Route path='/' exact component={StartPage} />
+          <Route path='/bikes' component={BikesPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/login' render={() => <LogInPage />} />
+          <Route path='/admin' render={() => <AdminPage {...this.state} />} />
+          <Route component={ErrorPage} />
+        </Switch>
 
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Content;
