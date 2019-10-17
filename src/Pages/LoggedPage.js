23e2546122ee8334c86ee14573f81567468ec0b0
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 12:25:47 +0100

    provide component LoggedPage with props login and password to display after login

--- src/Pages/LoggedPage.js
+++ src/Pages/LoggedPage.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const LoggedPage = (props) => {
-  const { name, password } = props;
+  const { login, password } = props;
 
   return (
     <div className="logged-page">
       <h2 className="logged-page__header">Admin Page</h2>
       <p className="logged-page__paragraph">You have successfully logged</p>
-      <p className="logged-page__paragraph"><span>Login: </span>{name}</p>
+      <p className="logged-page__paragraph"><span>Login: </span>{login}</p>
       <p className="logged-page__paragraph"><span>Password: </span>{password}</p>
     </div>
   );
 }
 
 LoggedPage.propTypes = {
-  name: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  password: PropTypes.string,
 }
 
 export default LoggedPage;
