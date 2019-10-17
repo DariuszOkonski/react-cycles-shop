e823f12fe0109aa44bf059e064168cd9c6ac686a
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 12:27:45 +0100

    provide component LogInPage with props login, password, onHandleLogin, onHandlePassword, onHandleSubmint to control component Content

--- src/Pages/LogInPage.js
+++ src/Pages/LogInPage.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const LogInPage = () => {
+const LogInPage = (props) => {
+  const { login, password, onHandleLogin, onHandlePassword, onHandleSubmit } = props;
+
   return (
-    <form className="login-page">
+    <form onSubmit={onHandleSubmit} className="login-page">
       <h2>Log In to Admin Panel</h2>
 
       <label htmlFor="login">Login:</label>
-      <input type="text" name="login" />
+      <input
+        value={login}
+        onChange={(e) => onHandleLogin(e)}
+        type="text"
+        name="login"
+        placeholder="Type anything as a login..."
+      />
 
       <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="" />
+      <input
+        value={password}
+        onChange={(e) => onHandlePassword(e)}
+        type="password"
+        name="password"
+        placeholder="Type anything as a password..."
+      />
 
       <button type="submit">Submit</button>
     </form>
