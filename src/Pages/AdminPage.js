22a3a729635f917e8cc6d372b73e8c218e2aa0fb
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 10:57:09 +0100

    recive props name, password and isLogged from Content component and send it to LoggedPage component

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom'
 import LoggedPage from './LoggedPage';
 
-const AdminPage = ({ logged }) => {
+
+const AdminPage = ({ name, password, isLogged }) => {
   return (
     <div className="admin-page">
-      {logged ? <LoggedPage /> : <Redirect to='/login' />}
+      {isLogged ?
+        <LoggedPage
+          name={name}
+          password={password}
+        />
+        :
+        <Redirect
+          to='/login'
+        />}
     </div>
   );
+}
+
+AdminPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  isLogged: PropTypes.bool.isRequired
 }
 
 export default AdminPage;
