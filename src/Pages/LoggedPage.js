92852ec8a8d53140470c7ce00c1ba1571b55b9f3
Dariusz Okonski <darek200180@gmail.com>
2019-10-17 10:54:26 +0100

    send props name and password to LoggedPage component and add PropTypes

--- src/Pages/LoggedPage.js
+++ src/Pages/LoggedPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const LoggedPage = () => {
+const LoggedPage = (props) => {
+  const { name, password } = props;
+
   return (
     <div className="logged-page">
       <h2 className="logged-page__header">Admin Page</h2>
       <p className="logged-page__paragraph">You have successfully logged</p>
-      <p className="logged-page__paragraph"><span>Login:</span> John</p>
-      <p className="logged-page__paragraph"><span>Password:</span> abc</p>
+      <p className="logged-page__paragraph"><span>Login: </span>{name}</p>
+      <p className="logged-page__paragraph"><span>Password: </span>{password}</p>
     </div>
   );
+}
+
+LoggedPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 }
 
 export default LoggedPage;
