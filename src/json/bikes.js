cfce4d2c9115cebc96126db960b5ea9aa970765b
Dariusz Okonski <darek200180@gmail.com>
2019-10-19 22:19:58 +0100

    add file bike.js to simulate database

---
+++ src/json/bikes.js
@@ -0,0 +1,69 @@
+import giantRoad from '../img/bikes/road-bikes/giant-road.jpg';
+import specializedRoad from '../img/bikes/road-bikes/specialized-road.jpg';
+import trekRoad from "../img/bikes/road-bikes/trek-road.jpg";
+import cubeRoad from "../img/bikes/road-bikes/cube-road.jpg";
+import scottMountain from "../img/bikes/mountain-bikes/scott-mountain.jpg";
+import cannondaleMountain from "../img/bikes/mountain-bikes/cannondale-mountain.jpg";
+import bianchiMountain from "../img/bikes/mountain-bikes/bianchi-mountain.jpg";
+
+// export const types = ['road bikes', 'mountain bikes'];
+
+export const bikes = [{
+    id: "1",
+    type: "road bikes",
+    make: "giant",
+    img: giantRoad,
+    description: "lorem ipsum hello world...",
+    price: '$345'
+  },
+  {
+    id: "2",
+    type: "road bikes",
+    make: "specialized",
+    img: specializedRoad,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$445'
+  },
+  {
+    id: "3",
+    type: "road bikes",
+    make: "trek",
+    img: trekRoad,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$635'
+  },
+  {
+    id: "4",
+    type: "road bikes",
+    make: "cube",
+    img: cubeRoad,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$542'
+  },
+  {
+    id: "5",
+    type: "mountain bikes",
+    make: "scott",
+    img: scottMountain,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$621'
+  },
+  {
+    id: "6",
+    type: "mountain bikes",
+    make: "cannondale",
+    img: cannondaleMountain,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$443'
+  },
+  {
+    id: "7",
+    type: "mountain bikes",
+    make: "bianchi",
+    img: bianchiMountain,
+    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    price: '$375'
+  },
+];
+
+export default bikes;
