cf56f1c9696e5acbbe238e26b9cfe4e0e7d67aac
Dariusz Okonski <darek200180@gmail.com>
2019-10-19 22:17:29 +0100

    add Link to display list of bike types and make paths to it

--- src/Pages/BikesPage.js
+++ src/Pages/BikesPage.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import types from '../json/bikes';
 
 const BikesPage = () => {
+
+  const typeList = types.map(type => type.type);
+  const uniqueTypes = typeList.filter((item, pos) => typeList.indexOf(item) === pos);
+
   return (
-    <div className="bikes-page">BikesPage</div>
+    <div>
+      <h1>List of bikes</h1>
+      <ul>
+        {uniqueTypes.map(item => <li key={item}>
+          <Link to={`/products/${item}`}>{item}</Link>
+        </li>)}
+      </ul>
+    </div>
   );
 }
 
