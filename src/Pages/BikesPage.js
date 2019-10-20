76b5da4c0f3684dd7762e04e5c6839bf9b1f1d23
Dariusz Okonski <darek200180@gmail.com>
2019-10-20 14:07:39 +0100

    add classes for styling to BikesPage component

--- src/Pages/BikesPage.js
+++ src/Pages/BikesPage.js
@@ -8,12 +8,19 @@ const BikesPage = () => {
   const uniqueTypes = typeList.filter((item, pos) => typeList.indexOf(item) === pos);
 
   return (
-    <div>
-      <h1>List of bikes</h1>
-      <ul>
-        {uniqueTypes.map(item => <li key={item}>
-          <Link to={`/products/${item}`}>{item}</Link>
-        </li>)}
+    <div className="bikes-page">
+      <h2 className="bikes-page__header">List of Bikes</h2>
+      <ul className="bikes-page__items">
+        {uniqueTypes.map(item =>
+          <li
+            className="bikes-page__item"
+            key={item}
+          >
+            <Link
+              className="bikes-page__link"
+              to={`/products/${item}`}
+            >{item}</Link>
+          </li>)}
       </ul>
     </div>
   );
