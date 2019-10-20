82a47b7c454f97ed3dd55424b29f288136d66d38
Dariusz Okonski <darek200180@gmail.com>
2019-10-20 14:43:45 +0100

    add new bikes with descriptions

--- src/json/bikes.js
+++ src/json/bikes.js
@@ -5,6 +5,19 @@ import cubeRoad from "../img/bikes/road-bikes/cube-road.jpg";
 import scottMountain from "../img/bikes/mountain-bikes/scott-mountain.jpg";
 import cannondaleMountain from "../img/bikes/mountain-bikes/cannondale-mountain.jpg";
 import bianchiMountain from "../img/bikes/mountain-bikes/bianchi-mountain.jpg";
+import scottElectric from '../img/bikes/electrict-bikes/scott-electrict.jpg';
+import giantElectric from '../img/bikes/electrict-bikes/giant-electrict.jpg';
+import trekElectric from '../img/bikes/electrict-bikes/trek-electrict.jpg';
+import giantHybrid from '../img/bikes/hybrid-bikes/giant-hybrid.jpg';
+import scottHybrid from '../img/bikes/hybrid-bikes/scott-hybrid.jpg';
+import cubeHybrid from '../img/bikes/hybrid-bikes/cube-hybrid.jpg';
+import trekHybrid from '../img/bikes/hybrid-bikes/trek-hybrid.jpg';
+import frogTouring from '../img/bikes/touring-bikes/frog-touring.jpg';
+import specializedTouring from '../img/bikes/touring-bikes/specialized-touring.jpg';
+import giantTouring from '../img/bikes/touring-bikes/giant-touring.jpg';
+import giantBMX from '../img/bikes/bmx-bikes/giant-bmx.jpg';
+import specializedBMX from '../img/bikes/bmx-bikes/specialized-bmx.jpg';
+import trekBMX from '../img/bikes/bmx-bikes/trek-bmx.jpg';
 
 // export const types = ['road bikes', 'mountain bikes'];
 
@@ -13,7 +26,7 @@ export const bikes = [{
     type: "road bikes",
     make: "giant",
     img: giantRoad,
-    description: "lorem ipsum hello world...",
+    description: "Road bikes are built for performance. From hilly sportives and group rides, to criterium races and Ironman podiums - road and triathlon bikes are aerodynamic, lightweight and efficient: designed for all out on-road speed in training and competition. With a wide range of road bikes featuring cutting edge aerodynamics, carbon fibre and aluminium construction, disc brakes and endurance or full on race geometry – we have the right bike for every rider.",
     price: '$345'
   },
   {
@@ -21,7 +34,7 @@ export const bikes = [{
     type: "road bikes",
     make: "specialized",
     img: specializedRoad,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: 'Specialized has become one of the most recognized brands in the world of road cycling. A host of professional teams choose Specialized road bikes as their weapon of choice, including many Tour De France Champions. Choose from the diverse Specialized Range to find the Specialized road bike that best suits your needs, from the comfort of the Specialized Roubaix, to the aerodynamic speed of the Specialized Venge',
     price: '$445'
   },
   {
@@ -29,7 +42,7 @@ export const bikes = [{
     type: "road bikes",
     make: "trek",
     img: trekRoad,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: "Trek is the #1 performance bike brand in the world. Our R&D department packs more engineering horsepower than any other bike company, and they're committed to making every one of our road bikes, from hand-built carbon race machines to refined aluminium models, the absolute best in its class. You'll feel the difference, from the first ride all the way to the podium.",
     price: '$635'
   },
   {
@@ -37,7 +50,7 @@ export const bikes = [{
     type: "road bikes",
     make: "cube",
     img: cubeRoad,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: "A CUBE bike with the SL tag has to earn its place in the line-up. With race-inspired geometry, a super-stiff carbon chassis that still manages to keep the rider comfortable for long days in the saddle, and a full complement of Shimano's utterly gorgeous Ultegra Di2 components for intuitive shifting and powerful braking, the Agree C:62 SL is a state of the art machine capable of transforming every ounce of effort into the pedals into blistering straight-line speed... all day long. Whether mixing it up in the peloton or hammering out the miles on a summer club training ride, the Agree C:62's raison d'etre can be summed up in two words: maximum velocity.",
     price: '$542'
   },
   {
@@ -45,7 +58,7 @@ export const bikes = [{
     type: "mountain bikes",
     make: "scott",
     img: scottMountain,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: "Our mountain bikes are the weapons of choice for many top level riders from around the world and they have won multiple World and Olympic titles. But we don’t rest on those successes, we continue to create and develop the best MTBs in the world. And not only for good riders but for anyone who wants to be on a bike. You can be a cross country enthusiast, a trail lover, an enduro devotee, a crazy downhiller or a fan of electric mountain bikes, you will find the perfect MTB in our range. ",
     price: '$621'
   },
   {
@@ -53,7 +66,7 @@ export const bikes = [{
     type: "mountain bikes",
     make: "cannondale",
     img: cannondaleMountain,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: "Cannondale bikes have been a firm favourite here at Wheelbase for many years, not only do they offer outstanding bikes across all spectrums of cycling to suit any user, but boast some serious market leaders and award winners.",
     price: '$443'
   },
   {
@@ -61,8 +74,112 @@ export const bikes = [{
     type: "mountain bikes",
     make: "bianchi",
     img: bianchiMountain,
-    description: 'loremkas jf lksjflkjao aflkasjdf a aflijdo a',
+    description: "Bianchi mountain bikes encompass the racing spirit that Bianchi has cultivated over the 130 years they’ve been building bikes. They offer their MTBs with both lightweight alloy frames and super lightweight carbon, all with price points that cater to a variety of different budgets.",
     price: '$375'
+  },
+  {
+    id: "8",
+    type: "electric bikes",
+    make: "scott",
+    img: scottElectric,
+    description: "If you are looking for all the advantage of cycling but with a little extra assistance, our electric bikes are made for you. Our range extends from electric mountain bikes, trekking e-bikes, hybrid electric bikes to urban electric bikes. There are models for men, women and even kids. You will definitely find the perfect bike for your needs. Time to go further and easier.",
+    price: '$945'
+  },
+  {
+    id: "9",
+    type: "electric bikes",
+    make: "giant",
+    img: giantElectric,
+    description: "Giant electric bikes are designed to let you travel further and faster with greater ease, allowing you to still enjoy the experience of cycling, but providing a little extra assistance when it’s needed. Powered by our innovative SyncDrive motor, designed in collaboration with Yamaha, every Giant e-bike offers top in-class performance and efficiency.",
+    price: '$921'
+  },
+  {
+    id: "10",
+    type: "electric bikes",
+    make: "trek",
+    img: trekElectric,
+    description: "Electric bikes amplify your pedalling power and your ability to do and see more. Trek e-bikes are quick and smooth, with predictable, easy-to-control power and a long-lasting removable battery that recharges at any household outlet.",
+    price: '$967'
+  },
+  {
+    id: "11",
+    type: "hybrid bikes",
+    make: "giant",
+    img: giantHybrid,
+    description: "Cycle to work, ride for fitness and fun. Hybrid bikes are built for the city, bike paths and the open road - featuring a blend of road bike efficiency with mountain bike stability and control",
+    price: '$789'
+  },
+  {
+    id: "12",
+    type: "hybrid bikes",
+    make: "scott",
+    img: scottHybrid,
+    description: "Scott hybrid bikes take on the city streets and canal paths. Comfortable riding positions and versatile components mean you can take your Scott hybrid bike near anywhere you want. There are both men’s and women’s versions available, suiting all riders.",
+    price: '$654'
+  },
+  {
+    id: "13",
+    type: "hybrid bikes",
+    make: "cube",
+    img: cubeHybrid,
+    description: "Cube electric bikes are designed to let you travel further and faster with greater ease, allowing you to still enjoy the experience of cycling, but providing a little extra assistance when it’s needed. Powered by our innovative SyncDrive motor.",
+    price: '$876'
+  },
+  {
+    id: "14",
+    type: "hybrid bikes",
+    make: "trek",
+    img: trekHybrid,
+    description: "Trek's line-up includes gender-neutral and women’s models, which have touchpoints like saddles and handlebars that can provide a better fit and feel to some women from the start. Every model is held to the same high standard of comfort and performance. No two riders are exactly the same, and that's why we have options. But every rider deserves a bike that fits and feels great, no matter what their gender, body type, style of riding or level of experience.",
+    price: '$923'
+  },
+  {
+    id: "15",
+    type: "touring bikes",
+    make: "frog",
+    img: frogTouring,
+    description: "Frog touring bikes take on the city streets and canal paths. Comfortable riding positions and versatile components mean you can take your Scott hybrid bike near anywhere you want. There are both men’s and women’s versions available, suiting all riders.",
+    price: '$432'
+  },
+  {
+    id: "16",
+    type: "touring bikes",
+    make: "specialized",
+    img: specializedTouring,
+    description: "Specialized touring bikes are designed to let you travel further and faster with greater ease, allowing you to still enjoy the experience of cycling, but providing a little extra assistance when it’s needed. Powered by our innovative SyncDrive motor.",
+    price: '$456'
+  },
+  {
+    id: "17",
+    type: "touring bikes",
+    make: "giant",
+    img: giantTouring,
+    description: "Giant touring bikes are designed to let you travel further and faster with greater ease, allowing you to still enjoy the experience of cycling, but providing a little extra assistance when it’s needed. Powered by our innovative SyncDrive motor, designed in collaboration with Yamaha, every Giant e-bike offers top in-class performance and efficiency.",
+    price: '$437'
+  },
+  {
+    id: "18",
+    type: "bmx bikes",
+    make: "giant",
+    img: giantBMX,
+    description: "From tricks to shredding dirt, these kids’ BMX bikes are all about action. Made with lightweight yet rugged frames and components, our collection of BMX bikes for kids is ready to rip.",
+    price: '$342'
+  },
+  {
+    id: "19",
+    type: "bmx bikes",
+    make: "specialized",
+    img: specializedBMX,
+    description: "Back in the early days of BMX, complete bikes were considered to be low-end options for beginner riders. That is no longer the case and many companies now offer quality “completes” that use quality components.",
+    price: '$334'
+  },
+  {
+    id: "20",
+    type: "bmx bikes",
+    make: "trek",
+    img: trekBMX,
+    description: "From tricks to shredding dirt, these kids’ BMX bikes are all about action.",
+    price: '$367'
   },
 ];
 
